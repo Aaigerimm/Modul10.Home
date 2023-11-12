c47bfc5f1a14c27ebd19cdf457cc37c1d99b9ab9
nurik0708 <aaikossya09@gmail.com>
2023-11-12 22:50:10 +0600

    Добавьте файлы проекта.

---
+++ Modul10.Home/ICalculatable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul10.Home
+{
+    public interface ICalculatable
+    {
+        double Plus(double a, double b);
+        double Umnozhit(double a, double b);
+        double Minus(double a, double b);
+        double Delit(double a, double b);
+    }
+
+    public class Calculator : ICalculatable
+    {
+        public double Plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Umnozhit(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Delit(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
+    }
+
+    internal class Calculatable
+    {
+        static void Main()
+        {
+            ICalculatable calculatable = new Calculator();
+            double rslt1 = calculatable.Plus(7, 14);
+            double rslt2 = calculatable.Umnozhit(19, 3);
+            double rslt3 = calculatable.Minus(4, 8);
+            double rslt4 = calculatable.Delit(40, 4);
+            Console.WriteLine(rslt1);
+            Console.WriteLine(rslt2);
+            Console.WriteLine(rslt3);
+            Console.WriteLine(rslt4);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Modul10.Home/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul10.Home
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"Full name: {FirstName} {LastName}");
+        }
+
+        public override string ToString() => $"{FirstName} {LastName}";
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person other)
+            {
+                return FirstName == other.FirstName && LastName == other.LastName;
+            }
+            return false;
+        }
+    }
+
+    public class Student : Person
+    {
+        public List<Teacher> Teachers { get; } = new List<Teacher>();
+
+        public Student(string firstName, string lastName) : base(firstName, lastName)
+        {
+        }
+
+        public void AddTeacher(Teacher teacher)
+        {
+            Teachers.Add(teacher);
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Student name:{FirstName} {LastName}");
+            Console.WriteLine("Teachers : ");
+            foreach (var teacher in Teachers)
+            {
+                Console.WriteLine(teacher);
+            }
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public List<Student> Students { get; } = new List<Student>();
+
+        public Teacher(string firstName, string lastName) : base(firstName, lastName)
+        {
+        }
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+            student.AddTeacher(this);
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Teacher: {FirstName} {LastName}");
+            Console.WriteLine("Students:");
+            foreach (var student in Students)
+            {
+                Console.WriteLine("  {student}");
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Bolatbek", "Aigerim");
+            Student student = new Student("Bolatbek", "Aigerim");
+            Teacher teacher = new Teacher("Rakhimova", "Ekaterina");
+            student.AddTeacher(teacher);
+            teacher.AddStudent(student);
+
+            List<Person> people = new List<Person> { person, student, teacher };
+
+            foreach (var person1 in people)
+            {
+                person.Print();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
